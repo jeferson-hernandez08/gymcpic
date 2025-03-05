96485610a580aeffba076bc286e15c1c13261637
Jeferson <jefer.hernandez1@gmail.com>
2025-03-04 23:17:44 -0500

    Session video de formacion min 22 min

--- app/config/global.php
+++ app/config/global.php
@@ -1,6 +1,16 @@
 <?php
-define("MAIN_APP_ROUTE", __DIR__.'/../app/');
 
+// define("MAIN_APP_ROUTE", __DIR__.'/../app/');
+// define("DRIVER", 'mysql');
+// define("HOST", 'localhost');
+// define("USERNAME_DB", 'root');
+// define("PASSWORD_DB", '');
+// define("DATABASE", 'gym_cpic');
+// define("CHARSET", 'utf8mb4');
+// define("COLLATION", 'utf8mb4_unicode_ci');
+
+
+define("MAIN_APP_ROUTE", __DIR__.'/../app/');
 define("DRIVER", 'mysql');
 define("HOST", 'localhost');
 define("USERNAME_DB", 'root');
